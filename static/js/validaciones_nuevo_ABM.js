b5ba53a4ba2aaaff895bec09de219fdc3036ed06
fedeel14 <federico.canziani@gmail.com>
2024-05-09 18:59:50 -0300

    Mejoro ab,

--- static/js/validaciones_nuevo_ABM.js
+++ static/js/validaciones_nuevo_ABM.js
@@ -7,7 +7,7 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
 	nombre: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	dni: /^.{6,14}$/, // 4 a 14 digitos.
+	dni: /^.{6,14}$/, // 4 a 12 digitos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 
@@ -18,8 +18,18 @@ const campos = {
 	apellido: false,
 	dni: false,
 	password: false,
-	email: false,
+	email: false
+
 }
+
+// Funcion para reiniciar los campos en false
+function reiniciarCampos() {
+    for (let campo in campos) {
+        campos[campo] = false;
+    }
+}
+
+//fin funcion
 
 // función tipo flecha de validacion
 
@@ -32,6 +42,7 @@ const validarCampo = (expresion, input, campo) => {
 		document.querySelector(`#form-${campo} i`).classList.remove('fa-circle-xmark');
 		document.querySelector(`#form-${campo} i`).classList.add('fa-circle-check');
 		document.querySelector(`#form-${campo} .error`).classList.remove('error-activo');
+		campos[campo] = true;
 
 	} else {
 		document.getElementById(`form-${campo}`).classList.remove('form-formulario-correcto');
@@ -39,6 +50,7 @@ const validarCampo = (expresion, input, campo) => {
 		document.querySelector(`#form-${campo} i`).classList.remove('fa-circle-check');
 		document.querySelector(`#form-${campo} i`).classList.add('fa-circle-xmark');
 		document.querySelector(`#form-${campo} .error`).classList.add('error-activo');
+		campos[campo] = false;
 	}
 
 }
@@ -48,26 +60,28 @@ const validarCampo = (expresion, input, campo) => {
 // Funcion valida password 2
 
 const validarPassword2 = () => {
-    const password1 = document.getElementById('usuario-password').value;
-    const password2 = document.getElementById('usuario-password2').value;
+	const password1 = document.getElementById('usuario-password').value;
+	const password2 = document.getElementById('usuario-password2').value;
 
 	// Debug, Muestro variables por pantalla.
 
 	// console.log('Contraseña 1:', password1);
-    // console.log('Contraseña 2:', password2);
+	// console.log('Contraseña 2:', password2);
 
-    if (password1 !== password2) {
-        document.getElementById('form-password2').classList.remove('form-formulario-correcto');
-        document.getElementById('form-password2').classList.add('form-formulario-error');
-        document.querySelector('#form-password2 i').classList.remove('fa-circle-check');
-        document.querySelector('#form-password2 i').classList.add('fa-circle-xmark');
-        document.querySelector('#form-password2 .error').classList.add('error-activo');
-    } else {
+	if (password1 !== password2 || !expresiones.password.test(password1)) {
+		document.getElementById('form-password2').classList.remove('form-formulario-correcto');
+		document.getElementById('form-password2').classList.add('form-formulario-error');
+		document.querySelector('#form-password2 i').classList.remove('fa-circle-check');
+		document.querySelector('#form-password2 i').classList.add('fa-circle-xmark');
+		document.querySelector('#form-password2 .error').classList.add('error-activo');
+		campos['password'] = false;
+	} else {
 		document.getElementById('form-password2').classList.remove('form-formulario-error');
-        document.getElementById('form-password2').classList.add('form-formulario-correcto');
+		document.getElementById('form-password2').classList.add('form-formulario-correcto');
 		document.querySelector('#form-password2 i').classList.remove('fa-circle-xmark');
-        document.querySelector('#form-password2 i').classList.add('fa-circle-check');
-        document.querySelector('#form-password2 .error').classList.remove('error-activo');
+		document.querySelector('#form-password2 i').classList.add('fa-circle-check');
+		document.querySelector('#form-password2 .error').classList.remove('error-activo');
+		campos['password'] = true;
 	}
 }
 
@@ -110,6 +124,7 @@ const validarFormulario = (e) => {
 
 		case "usuario-password2":
 			validarPassword2();
+
 			break;
 
 	}
@@ -118,13 +133,55 @@ const validarFormulario = (e) => {
 
 // Fin Funcion Validación Campo
 
-
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario); // cada vez que se presiona una tecla ejecuta el codigo
 	input.addEventListener('blur', validarFormulario); // blur cuando le damos click fuera del input se ejecuta.
 });
 
-// // Con esto cuando realizamos el submit no cambia la url. es para la demostración
-// formulario.addEventListener('submit', (e) => {
-// 	e.preventDefault();
-// });
+// cheque formulario 
+
+const check_inputs = (e) => {
+	e.preventDefault();
+
+	if (campos.nombre && campos.apellido && campos.dni && campos.password && campos.email ) {
+		// formulario.reset();
+		console.log("Funciona la función");
+		
+		// agrego clases al mesaje correcto
+		document.getElementById('form-mensaje-valido').classList.add('form-mensaje-valido-activo');
+		document.getElementById('form-valido-campo').classList.add('form-valido-campo-exito');
+		var mensajeExito = document.querySelector('.form-mensaje-valido');
+		mensajeExito.innerText = 'Formulario enviado exitosamente!';
+		mensajeExito.style.color = 'green';
+
+		// Funcion para que el mensaje quede 5 segundos
+		setTimeout(() => {
+			document.getElementById('form-mensaje-valido').classList.remove('form-mensaje-valido-activo');
+			document.getElementById('form-valido-campo').classList.remove('form-valido-campo-exito');
+		}, 3000); // queda 3 segundos el mensaje
+
+		// Reset del formulario si termina exitoso.
+		document.getElementById("formulario-usuario").reset();
+		
+		// Borra la clase de formulario correcto para volver a la case inicial.
+		document.querySelectorAll('.form-formulario').forEach((elemento) => {
+			elemento.classList.remove('form-formulario-correcto');
+		});
+		// reinicia los campos los pone en false.
+		reiniciarCampos();
+
+
+	} else {
+		document.getElementById('form-mensaje-valido').classList.add('form-mensaje-valido-activo');
+		document.getElementById('form-valido-campo').classList.add('form-valido-campo-error');
+		var mensajeExito = document.querySelector('.form-mensaje-valido');
+		mensajeExito.innerText = 'Completar formulario correctamente';
+		mensajeExito.style.color = 'red';
+		setTimeout(() => {
+			document.getElementById('form-mensaje-valido').classList.remove('form-mensaje-valido-activo');
+			document.getElementById('form-valido-campo').classList.remove('form-valido-campo-error');
+		}, 3000); // queda 3 segundos el mensaje.
+	}
+};
+
+// fin cheque coampo
