b64930f1a00d38ba4802aef1e323edaca0953967
dminiello <dminiello@gmail.com>
2024-06-29 10:47:46 -0300

    flask_app.py (con cambios) => funcional en Pythonanywhere

--- flask_app.py
+++ flask_app.py
@@ -24,10 +24,6 @@ app = Flask(__name__)
 # lo cual es necesario en muchos casos, como cuando se desarrolla una API que necesita ser consumida desde diferentes dominios.
 CORS(app)
 
-# @app.route('/')
-# def index():
-#     return 'Hola, mundo! Esta es mi primera aplicación Flask en PythonAnywhere.'
-
 # -------------------------------------------------------------------
 # Definimos la clase Peini
 # -------------------------------------------------------------------
@@ -39,65 +35,59 @@ class Peini:
         self.conn = mysql.connector.connect(
             host=host,
             user=user,
-            password=password)
-        self.cursor = self.conn.cursor(dictionary=True)  # dictionary=True para obtener los resultados como diccionarios (facilita la conversión a JSON)
+            password=password
+        )
+        self.cursor = self.conn.cursor()
 
         # Intentamos seleccionar la base de datos
         try:
-            self.cursor.execute(f"USE {database};")
+            self.cursor.execute(f"USE {database}")
         except mysql.connector.Error as err:
             # Si la base de datos no existe, la creamos
             if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
-                self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database};")
+                self.cursor.execute(f"CREATE DATABASE {database}")
                 self.conn.database = database
             else:
                 raise err
 
     # Crear la tabla de perfiles
-    def crear_tabla_perfiles(self):
         try:
             self.cursor.execute('''CREATE TABLE IF NOT EXISTS perfiles (
-                id_perfil TINYINT UNSIGNED AUTO_INCREMENT,
-                perfil VARCHAR(20) NOT NULL,
-                PRIMARY KEY (id_perfil));''')
+                id_perfil INT AUTO_INCREMENT PRIMARY KEY,
+                perfil VARCHAR(20) NOT NULL);''')
             self.conn.commit()
         except mysql.connector.Error as err:
             self.conn.rollback()
             raise err
-
+    
     # Insertar datos de ejemplo en la tabla de perfiles
-    def insertar_perfiles(self):
         try:
-            self.cursor.execute('''INSERT IGNORE INTO perfiles (perfil)
-                VALUES ('Admin'), ('Maestra/o');''')
+            self.cursor.execute('''INSERT IGNORE INTO perfiles (perfil) VALUES ('Admin'), ('Maestra/o');''')
             self.conn.commit()
         except mysql.connector.Error as err:
             self.conn.rollback()
             raise err
-        
+    
     # Crear la tabla de usuarios
-    def crear_tabla_usuarios(self):
         try:
             self.cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
-                firstname VARCHAR(50) NOT NULL,
-                lastname VARCHAR(50) NOT NULL,
+                nombre VARCHAR(50) NOT NULL,
+                apellido VARCHAR(50) NOT NULL,
                 dni INT UNSIGNED NOT NULL,
                 email VARCHAR(100) NOT NULL,
                 clave VARCHAR(255) NOT NULL,
                 id_usuario_perfil TINYINT UNSIGNED NOT NULL,
-                imagen BLOB,
-                PRIMARY KEY (dni),
-                FOREIGN KEY (id_usuario_perfil) REFERENCES perfiles(id_perfil)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;''')
+                imagen_url VARCHAR(255),
+                PRIMARY KEY (dni))''')
+    # FOREIGN KEY (id_usuario_perfil) REFERENCES perfiles(id_perfil))''')
             self.conn.commit()
         except mysql.connector.Error as err:
             self.conn.rollback()
             raise err
 
     # Insertar datos de ejemplo en la tabla de usuarios
-    def insertar_usuarios(self):
         try:
-            self.cursor.execute('''INSERT IGNORE INTO usuarios (firstname, lastname, dni, email, clave, id_usuario_perfil, imagen)
+            self.cursor.execute('''INSERT IGNORE INTO usuarios (nombre, apellido, dni, email, clave, id_usuario_perfil, imagen_url)
                 VALUES
                     ('Juan', 'Pérez', 11111111, 'juan@example.com', 'password123', 1, NULL),
                     ('María', 'González', 22222222, 'maria@example.com', 'securepwd', 2, NULL),
@@ -109,20 +99,19 @@ class Peini:
             self.conn.rollback()
             raise err
 
-    # Método para cerrar la conexión y el cursor
-    def close_connection(self):
+    # Cerramos el cursos inicial y abrimos uno nuevo con el parámetro dictionary=true
         self.cursor.close()
-        self.conn.close()
+        self.cursor = self.conn.cursor(dictionary=True) # Para obtener los resultados como diccionarios y facilitar la conversión a JSON
 
     # ---------------------------------------------------------------
     # Método para agregar un usuario a la base
     # ---------------------------------------------------------------
-    def agregar_usuario(self, firstname, lastname, dni, email, clave, id_usuario_perfil, imagen):
-        sql = "INSERT IGNORE INTO usuarios (firstname, lastname, dni, email, clave, id_usuario_perfil, imagen) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        valores = (firstname, lastname, dni, email, clave, id_usuario_perfil, imagen)
+    def agregar_usuario(self, nombre, apellido, dni, email, clave, id_usuario_perfil, imagen_url):
+        sql = "INSERT IGNORE INTO usuarios (nombre, apellido, dni, email, clave, id_usuario_perfil, imagen_url) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        valores = (nombre, apellido, dni, email, clave, id_usuario_perfil, imagen_url)
         self.cursor.execute(sql, valores)
         self.conn.commit()
-        return self.cursor.lastrowid  # Proporciona el valor de la clave primaria (generada automáticamente) por la base de datos para la fila recién insertada.
+        return self.cursor._last_insert_id # Proporciona el valor de la clave primaria por la base de datos para la fila recién insertada.
 
     # ---------------------------------------------------------------
     # Método para consultar un usuario por código
@@ -131,18 +120,18 @@ class Peini:
         self.cursor.execute(f"SELECT * FROM usuarios WHERE dni = {dni}")
         return self.cursor.fetchone()  # fetchone devuelve un solo registro
 
-     # ---------------------------------------------------------------
-     # Método para modificar los detalles de un usuario
-     # ---------------------------------------------------------------
-    def modificar_usuario(self, dni, nuevo_firstname, nuevo_lastname, nuevo_email, nuevo_clave, nuevo_id_usuario_perfil, nuevo_imagen):
-        sql = "UPDATE usuarios SET firstname = %s, lastname = %s, email = %s, clave = %s, id_usuario_perfil = %s, imagen = %s WHERE dni = %s"
-        valores = (nuevo_firstname, nuevo_lastname, nuevo_email, nuevo_clave, nuevo_id_usuario_perfil, nuevo_imagen, dni)
+    # ---------------------------------------------------------------
+    # Método para modificar los detalles de un usuario
+    # ---------------------------------------------------------------
+    def modificar_usuario(self, dni, nuevo_nombre, nuevo_apellido, nuevo_email, nuevo_clave, nuevo_id_usuario_perfil, nuevo_imagen_url):
+        sql = "UPDATE usuarios SET nombre = %s, apellido = %s, email = %s, clave = %s, id_usuario_perfil = %s, imagen_url = %s WHERE dni = %s"
+        valores = (nuevo_nombre, nuevo_apellido, nuevo_email, nuevo_clave, nuevo_id_usuario_perfil, nuevo_imagen_url, dni)
         self.cursor.execute(sql, valores)
         self.conn.commit()
         return self.cursor.rowcount > 0  # rowCount() devuelve el número de filas afectadas por la consulta
 
     # ---------------------------------------------------------------
-    # Método para listar todos los usuarios
+    # Método para listar a todos los usuarios
     # ---------------------------------------------------------------
     def listar_usuarios(self):
         self.cursor.execute("SELECT * FROM usuarios")
@@ -157,23 +146,6 @@ class Peini:
         self.conn.commit()
         return self.cursor.rowcount > 0  # rowCount() devuelve el número de filas afectadas por la consulta
 
-    # ---------------------------------------------------------------
-    # Método para mostrar los detalles de un usuario por DNI
-    # ---------------------------------------------------------------
-    def mostrar_usuario(self, dni):
-        usuario = self.consultar_usuario(dni)
-        if usuario:
-            print("-" * 50)
-            print(f"Nombre......: {usuario['firstname']}")
-            print(f"Apellido....: {usuario['lastname']}")
-            print(f"Documento...: {usuario['dni']}")
-            print(f"Correo......: {usuario['email']}")
-            print(f"Perfil......: {usuario['id_usuario_perfil']}")
-            print(f"Imagen......: {usuario['imagen']}")
-            print("-" * 50)
-        else:
-            print("Usuario no encontrado.")
-
 # -------------------------------------------------------------------
 # Cuerpo del programa
 # -------------------------------------------------------------------
@@ -182,18 +154,18 @@ peini = Peini(host='CaC2024PEINI.mysql.pythonanywhere-services.com', user='CaC20
 # Carpeta para guardar las imágenes.
 RUTA_DESTINO = '/home/CaC2024PEINI/mysite/static/img/'
 
-# #--------------------------------------------------------------------
-# # Listar todos los usuarios
-# #--------------------------------------------------------------------
-@app.route("/gestion_usuario", methods=["GET"]) #GET: método para obtener respuestas a nuestras peticiones.
+#--------------------------------------------------------------------
+# Listar todos los usuarios
+#--------------------------------------------------------------------
+@app.route("/usuarios", methods=["GET"]) #GET: método para obtener respuestas a nuestras peticiones.
 def listar_usuarios():
     usuarios = peini.listar_usuarios()
     return jsonify(usuarios)
 
 #--------------------------------------------------------------------
-# Mostrar un sólo usuario según su DNI
+# Mostrar sólo un usuario según su DNI
 #--------------------------------------------------------------------
-@app.route("/gestion_usuario/<int:dni>", methods=["GET"])
+@app.route("/usuarios/<int:dni>", methods=["GET"])
 def mostrar_usuario(dni):
     usuario = peini.consultar_usuario(dni)
     if usuario:
@@ -204,39 +176,39 @@ def mostrar_usuario(dni):
 #--------------------------------------------------------------------
 # Agregar un usuario
 #--------------------------------------------------------------------
-@app.route("/creacion_usuario", methods=["POST"])
+@app.route("/usuarios", methods=["POST"])
 def agregar_usuario():
-    nombre = request.form['firstname']
-    apellido = request.form['lastname']
-    documento = request.form['dni']
-    imagen = request.files['imagen']
-    correo = request.form['email']
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    dni = request.form['dni']
+    imagen_url = request.files['imagen_url']
+    email = request.form['email']
     perfil = request.form['id_usuario_perfil']
-    nombre_imagen = secure_filename(imagen.filename)
+    nombre_imagen = secure_filename(imagen_url.filename)
     nombre_base, extension = os.path.splitext(nombre_imagen)
     nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-    nuevo_dni = peini.agregar_usuario(nombre, apellido, documento, correo, perfil, imagen)
+    nuevo_dni = peini.agregar_usuario(nombre, apellido, dni, email, perfil, imagen_url)
     if nuevo_dni:
-        imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
-        return jsonify({"mensaje": "Usuario agregado correctamente.", "dni": nuevo_dni, "imagen": nombre_imagen}), 201
+        imagen_url.save(os.path.join(RUTA_DESTINO, nombre_imagen))
+        return jsonify({"mensaje": "Usuario agregado correctamente.", "dni": nuevo_dni, "imagen_url": nombre_imagen}), 201
     else:
         return jsonify({"mensaje": "Error al agregar al usuario."}), 500
 
 #--------------------------------------------------------------------
 # Modificar un usuario según su dni
 #--------------------------------------------------------------------
-@app.route("/gestion_usuario/<int:dni>", methods=["PUT"])
+@app.route("/usuarios/<int:dni>", methods=["PUT"])
 def modificar_usuario(dni):
-    nuevo_nombre  = request.form.get("firstname")
-    nuevo_apellido = request.form.get("lastname")
+    nuevo_nombre  = request.form.get("nombre")
+    nuevo_apellido = request.form.get("apellido")
     nuevo_email = request.form.get("email")
     nuevo_perfil = request.form.get("id_usuario_perfil")
-    if 'imagen' in request.files:
-        imagen = request.files['imagen']
-        nombre_imagen = secure_filename(imagen.filename)
+    if 'imagen_url' in request.files:
+        imagen_url = request.files['imagen_url']
+        nombre_imagen = secure_filename(imagen_url.filename)
         nombre_base, extension = os.path.splitext(nombre_imagen)
         nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
-        imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
+        imagen_url.save(os.path.join(RUTA_DESTINO, nombre_imagen))
         usuario = peini.consultar_usuario(dni)
         if usuario:
             imagen_vieja = usuario["imagen_url"]
@@ -248,14 +220,14 @@ def modificar_usuario(dni):
         if usuario:
             nombre_imagen = usuario["imagen_url"]
     if peini.modificar_usuario(dni, nuevo_nombre, nuevo_apellido, nuevo_email, nombre_imagen, nuevo_perfil):
-        return jsonify({"mensaje": "Usuario modificado"}), 200
+        return jsonify({"mensaje": "Usuario modificado."}), 200
     else:
-        return jsonify({"mensaje": "Usuario no encontrado"}), 403
+        return jsonify({"mensaje": "Usuario no encontrado."}), 403
 
 #--------------------------------------------------------------------
 # Eliminar un usuario según su dni
 #--------------------------------------------------------------------
-@app.route("/gestion_usuario/<int:dni>", methods=["DELETE"])
+@app.route("/usuarios/<int:dni>", methods=["DELETE"])
 def eliminar_usuario(dni):
     usuario = peini.consultar_usuario(dni)
     if usuario:
@@ -274,11 +246,4 @@ def eliminar_usuario(dni):
 # Configurar y ejecutar la aplicación Flask
 # -------------------------------------------------------------------
 if __name__ == '__main__':
-    
-    peini.crear_tabla_perfiles()
-    peini.insertar_perfiles()
-    peini.crear_tabla_usuarios()
-    peini.insertar_usuarios()
-
-    # Ejecutar la aplicación Flask
     app.run(debug=True)
